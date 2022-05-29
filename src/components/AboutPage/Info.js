251b94a6cca9fd8fe47a9c0faad2a4d19e373db2
AizadaSh <shabieva.ai@gmail.com>
2022-05-29 16:23:24 +0600

    created components

---
+++ src/components/AboutPage/Info.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from '../Title';
+import aboutBcg from '../../images/aboutBcg.jpeg';
+
+import React from 'react'
+
+export default function Info() {
+  return (
+    <div>Info</div>
+  )
+}
